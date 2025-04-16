b267399e21500069865ef9c86f983392df4dedd1
Alejogvy <Byui15.>
2025-04-16 00:08:04 -0500

    commit

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(passport.session());
 
 // Headers CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://cse341-crud-uv92.onrender.com");
+  res.setHeader("Access-Control-Allow-Origin", "https://productinventory-90te.onrender.com");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Z-Key");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, OPTIONS, DELETE");
